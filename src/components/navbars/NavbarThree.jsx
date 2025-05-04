af174a427ba1f59f292cb5b792af4b9ea4dcd5b5
Haris531 <151965404+Haris531@users.noreply.github.com>
2025-05-04 22:44:57 +0500

    change in navbar three

--- src/components/navbars/NavbarThree.jsx
+++ src/components/navbars/NavbarThree.jsx
@@ -5,14 +5,13 @@ export default function NavbarThree() {
         <>
             <div className="bg-gray-800 text-white ">
                 <div className="flex justify-end items-center mr-12 px-4 py-2 text-sm">
-                    <span>Email: harisahmad@1234.com</span>
+                    <span>Addres: Peshawer</span>
+                    <span className='ml-4'>Email: harisahmad@1234.com</span>
                     <span className="ml-4">Phone: 123-456-7890</span>
                 </div>
             </div>
-
-
             <nav className="bg-white ">
-                <div className="px-8 py-4 flex items-center">
+                <div className="px-8 py-4 flex justify-center items-center">
                     <div>
                         <a href="#">
                             <img src="" alt="Logo" classNameName="h-8 w-auto" />
