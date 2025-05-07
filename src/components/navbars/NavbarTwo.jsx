ef5c4d0c2b84532740fda209fcc5333865cf98f2
Haris531 <151965404+Haris531@users.noreply.github.com>
2025-05-07 20:19:29 +0500

    responsive work in navbar two

--- src/components/navbars/NavbarTwo.jsx
+++ src/components/navbars/NavbarTwo.jsx
@@ -10,14 +10,14 @@ export default function NavbarTwo() {
   };
 
   return (
-    <header className="bg-white shadow-md px-4 md:px-8 py-4 relative z-50">
+    <header className="bg-white shadow-md px-4 md:px-8 py-4 relative">
       <div className="flex items-center justify-between">
         <div>
           <a href="#">
             <img src="" alt="Logo" className="h-8 w-auto" />
           </a>
         </div>
-        <nav className="hidden text-sm lg:text-base gap-6 md:flex lg:gap-8 text-gray-700 font-medium">
+        <nav className="hidden md:flex text-sm lg:text-base gap-6 lg:gap-8 text-gray-700 font-medium">
           <a href="#" className="hover:text-blue-600">Home</a>
           <a href="#" className="hover:text-blue-600">About</a>
           <a href="#" className="hover:text-blue-600">Services</a>
@@ -26,7 +26,7 @@ export default function NavbarTwo() {
         </nav>
         <div className="relative hidden sm:flex items-center ml-4">
           <input type="text" placeholder="Search..."
-            className="px-2 py-2 w-36 sm:w-48 md:w-56 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-black"/>
+            className="px-2 py-2 w-36 sm:w-48 md:w-56 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-black" />
           <CiSearch className="absolute right-3 text-gray-500 text-xl cursor-pointer" />
         </div>
         <div className="md:hidden ml-4">
@@ -44,7 +44,7 @@ export default function NavbarTwo() {
 
         <div className="relative mt-2 flex items-center">
           <input type="text" placeholder="Search..."
-            className="px-4 py-2 w-full border border-gray-300 focus:outline-none focus:ring-1 focus:ring-black"/>
+            className="px-4 py-2 w-full border border-gray-300 focus:outline-none focus:ring-1 focus:ring-black" />
           <CiSearch className="absolute right-3 text-gray-500 text-xl cursor-pointer" />
         </div>
       </div>
