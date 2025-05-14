a29bb018a3e39885ccaa8bbb3b25eb0445fd5a70
Haris531 <151965404+Haris531@users.noreply.github.com>
2025-05-14 15:25:53 +0500

    hero section one is done but not completely

--- src/components/hero-sections/HeroOne.jsx
+++ src/components/hero-sections/HeroOne.jsx
@@ -3,7 +3,30 @@ import React from 'react'
 export default function HeroOne() {
   return (
     <>
-    <h1>Hero One</h1>
+    <section className="bg-white">
+      <div className="max-w-7xl mx-auto px-6 py-20 flex flex-col md:flex-row gap-12 items-center">
+        <div>
+          <h1 className="text-4xl sm:text-5xl font-extrabold text-gray-900 mb-6">
+            Elevate Your Brand <br /> with Stunning Web Design
+          </h1>
+          <p className="text-lg text-gray-600 mb-8">
+            We create responsive, beautiful, and user-friendly websites that help your business grow. Let's build something great together.
+          </p>
+          <div className="flex flex-wrap gap-4">
+            <a href="#contact" className="px-6 py-3 bg-blue-600 text-white rounded-2xl font-medium hover:bg-blue-700 transition">
+              Get Started
+            </a>
+            <a href="#portfolio" className="px-6 py-3 border border-gray-300 text-gray-700  rounded-2xl hover:bg-gray-100  transition">
+              View Portfolio
+            </a>
+          </div>
+        </div>
+        <div className="w-full">
+          <img src="https://plus.unsplash.com/premium_photo-1746718186768-95b366cc69ad?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Hero" className="w-full h-auto rounded-2xl shadow-xl"/>
+        </div>
+      </div>
+    </section>
     </>
   )
 }
--- src/pages/AllHeroSections.jsx
+++ src/pages/AllHeroSections.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import HeroOne from '../components/hero-sections/HeroOne'
 
 export default function AllHeroSections() {
   return (
-   <>
-   <h1>All Hero Sections</h1>
-   </>
+    <>
+      <div className=''>
+        <div className='space-y-4'>
+          <div><HeroOne /></div>
+        </div>
+      </div>
+    </>
   )
 }
