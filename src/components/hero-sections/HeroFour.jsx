f0720f4804196622c373aaa6c33515727872e90e
Haris531 <151965404+Haris531@users.noreply.github.com>
2025-05-21 00:19:32 +0500

    changes in gero section four

--- src/components/hero-sections/HeroFour.jsx
+++ src/components/hero-sections/HeroFour.jsx
@@ -7,19 +7,20 @@ export default function HeroFour() {
             <section className='bg-black w-full min-h-screen flex flex-col justify-center items-center'>
                 <div>
                     <div className='flex flex-col justify-center items-center'>
-                        <h2 className='text-gray-500 text-sm mb-2'>INFRASTRUCTURE</h2>
-                        <h1 className='text-white text-5xl text-center font-bold mb-6'>Build,host and scale <br /> your collaborative paroduct</h1>
+                        <div className='bg-slate-500 w-20 h-16 mb-4 herofour'></div>
+                        <h2 className='text-[#C0C0C0] text-sm mb-2'>INFRASTRUCTURE</h2>
+                        <h1 className='text-white text-5xl text-center font-bold mb-6'>Build,host, and scale <br /> your collaborative paroduct</h1>
                         <button className='px-4 py-2 bg-white text-black text-sm font-semibold'>
                             Sign up for free
                         </button>
                     </div>
                     <div className='mt-6 flex flex-row'>
-                        <div className="w-72 p-4 border border-gray-50 ">
+                        <div className="w-72 p-4 border border-gray-50">
                             <div className="w-8 h-8  text-blue-600  flex items-center">
                                 <FiInfo className="w-6 h-6" />
                             </div>
                             <h2 className="text-lg font-semibold text-white">Card Title</h2>
-                            <p className="text-gray-600">
+                            <p className="text-gray-400">
                                 This is a card component using a React icon, heading, and paragraph.
                             </p>
                         </div>
@@ -28,12 +29,12 @@ export default function HeroFour() {
                                 <FiInfo className="w-6 h-6" />
                             </div>
                             <h2 className="text-lg font-semibold text-white">Card Title</h2>
-                            <p className="text-gray-600">
+                            <p className="text-gray-400">
                                 This is a card component using a React icon, heading, and paragraph.
                             </p>
                         </div>
                           <div className="w-72 p-4 border border-gray-50 ">
-                            <div className="w-8 h-8  text-blue-600  flex items-center">
+                            <div className="w-8 h-8  text-blue-400  flex items-center">
                                 <FiInfo className="w-6 h-6" />
                             </div>
                             <h2 className="text-lg font-semibold text-white">Card Title</h2>
@@ -42,7 +43,7 @@ export default function HeroFour() {
                             </p>
                         </div>
                           <div className="w-72 p-4 border border-gray-50 ">
-                            <div className="w-8 h-8  text-blue-600  flex items-center">
+                            <div className="w-8 h-8  text-blue-400  flex items-center">
                                 <FiInfo className="w-6 h-6" />
                             </div>
                             <h2 className="text-lg font-semibold text-white">Card Title</h2>
