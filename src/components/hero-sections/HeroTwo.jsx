f5d1ddd837529c86132e73e59e2ee37e205482a1
Haris531 <151965404+Haris531@users.noreply.github.com>
2025-05-20 17:11:36 +0500

    changes in hero section two

--- src/components/hero-sections/HeroTwo.jsx
+++ src/components/hero-sections/HeroTwo.jsx
@@ -6,7 +6,7 @@ export default function HeroTwo() {
         <>
             <section className="bg-[url('/images/bgimage.avif')] bg-cover bg-center bg-no-repeat w-full min-h-screen flex flex-col items-center px-2 sm:px-6 py-40 sm:py-20">
                 <div>
-                    <p className="text-4xl sm:6xl font-bold mb-8">
+                    <p className="text-4xl sm:text-6xl font-bold mb-8">
                         Create better video, <br /> faster than ever
                     </p>
                     <div className="flex justify-center lg:justify-start text-base font-bold gap-6">
@@ -16,16 +16,19 @@ export default function HeroTwo() {
                         <span className="hover:underline decoration-yellow-500 decoration-2 underline-offset-4">Images</span>
                     </div>
                 </div>
-                <div className="mt-6 w-full max-w-lg ">
+                <div className="mt-6 w-full max-w-lg">
                     <div className="relative">
-                        <input type="text" placeholder="Search..."
-                            className="w-full sm:w-auto px-4 py-3 border border-gray-400 bg-white rounded-full focus:outline-none text-sm font-semibold"
+                        <input
+                            type="text"
+                            placeholder="Search..."
+                            className="w-full px-4 pr-12 py-3 border border-gray-300 bg-white rounded-full focus:outline-none text-sm font-semibold text-black"
                         />
-                        <div className="absolute inset-y-0 right-0 flex items-center text-white bg-yellow-500 p-2 cursor-pointer">
+                        <button className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-yellow-500 text-white p-3 rounded-full hover:bg-yellow-600 transition">
                             <CiSearch size={20} />
-                        </div>
+                        </button>
                     </div>
                 </div>
+
             </section>
 
         </>
